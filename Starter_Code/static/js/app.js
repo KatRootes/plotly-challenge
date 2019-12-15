bd567e786af7a3a3c7219404d521235d15ea1a17
Katherine <53545200+KatRootes@users.noreply.github.com>
2019-12-15 16:38:02 -0600

    Basic bar chart working.

--- Starter_Code/static/js/app.js
+++ Starter_Code/static/js/app.js
@@ -2,6 +2,10 @@
 // Create the data url
 const url = "./samples.json";
 
+var names = [];
+var metadata = [];
+var samples = [];
+
 /***************************** Functions *****************************/
 /**
  * Helper function to select data
@@ -38,10 +42,12 @@ function populateTestSubjectDropDown(names)
     });
 }
 
-// Update the Demographics
-function updateDemographics(demographics)
+// Update the Demographics information on screen
+function updateDemographics(name)
 {
-    console.log("demographics:  ", demographics);
+    demographics = selectDemographics(name);
+    // console.log("demographics:  ", demographics);
+
     // Select the element to insert demographics within
     let panelBody = d3.selectAll("#sample-metadata");
     panelBody.attr("value","Hello World");
@@ -49,7 +55,7 @@ function updateDemographics(demographics)
     // Clear existing metadata
     panelBody.html("");
 
-    console.log(demographics[0]);
+    // console.log(demographics[0]);
 
     // Populate the demographics
     //panelBody.append("table").attr("class","table table-striped");
@@ -70,35 +76,109 @@ function init()
         // console.log("Id: ", data[0].metadata.map(row => row.id));
         // var id = data[0].metadata.map(row => row.id);
 
-        console.log("names: ", data.names);
-        console.log("metadata: ", data.metadata);
+        names = data.names;
+        metadata = data.metadata;
+        samples = data.samples;
+        // console.log("names: ", data.names);
+        // console.log("metadata: ", data.metadata);
         console.log("samples: ", data.samples);
 
         // Grab the first id for initial dashboard
-        let name = data.names[0];
-        console.log("name in init: ", parseInt(name));
+        let subject = data.names[0];
+        console.log("name in init: ", parseInt(subject));
         console.log("metadata id: ", data.metadata[0].id);
-        let id = parseInt(name);
+        // let id = parseInt(name);
 
         populateTestSubjectDropDown(data.names);
-        let selection = data.metadata.filter(name => name.id === id);
-        console.log("selection: ", selection);
-        updateDemographics(selection);
+        // let selection = data.metadata.filter(name => name.id === id);
+        // console.log("selection: ", selection);
+        // updateDemographics(selection);
+        updateDemographics(subject);
+
+        initBarChart(subject);
     });
 
+}
+
+// Function to convert selection to integer and find demographics
+function selectDemographics(subject)
+{
+    let id = parseInt(subject);
+    let selection = metadata.filter(name => name.id === id);
+    // console.log("selection: ", selection);
+    return selection;
 }
 
 
 
 // Function to update the dashboard based on test subject selection
-function optionChanged(event)
+function optionChanged()
 {
-    console.log("updateDashboard: ", event);
+    let id = d3.selectAll("#selDataset").node().value;
+    // console.log("updateDashboard: ", id);
+    //let id = parseInt(value);
+    //console.log(id);
+    updateDemographics(id);
+}
+
+// Function to init the horizontal bar chart
+function initBarChart(subject)
+{
+    let selection = samples.filter(function(sample)
+    {
+        // console.log(`subject.id: ${sample.id} == subject: ${subject}`);
+        let id = parseInt(sample.id);
+        // console.log(subject.id == subject);
+        return sample.id == subject;
+    });
+    console.log("samples:  ", samples);
+    console.log("subject:  ", subject);
+    console.log("selction:  ", selection);
+    console.log("values:  ", selection[0].sample_values);
+    console.log("labels:  ", selection[0].otu_ids);
+
+    let pts = [];
+    for (var i=0; i<selection[0].sample_values.length; i++)
+    {
+        pts.push({"otu_id":`OTU ${selection[0].otu_ids[i]}`,
+                       "sample_value":selection[0].sample_values[i],
+                       "otu_label":selection[0].otu_labels[i] });
+    }
+
+    console.log(pts);
+
+    console.log(pts[0].sample_value, pts[1].sample_value);
+    let sortedByOTU = pts.sort((a, b) => b.sample_value - a.sample_value);
+    let sliced = sortedByOTU.slice(0,10);
+    let reversed = sliced.reverse();
+
+    console.log("sortedByOTU:  ", sortedByOTU);
+    console.log("sliced:  ", sliced);
+    console.log("reversed:  ", reversed);
+
+    var data = 
+    [{
+        x: reversed.map(object => object.sample_value),
+        y: reversed.map(object => object.otu_id),
+        text: reversed.map(object => object.otu_label),
+        mode: "bar",
+        type: "bar",
+        orientation: "h"
+    }];
+
+    var layout = 
+    {
+        title: `Top Ten OTUs for Subject ${subject}`,
+        height: 600,
+        width: 800  
+    };
+
+    Plotly.newPlot("bar", data, layout);
 }
 
 
 /*************************** Handlers *************************************/
-// Call updateDashboard() when a change takes place
+// Call optionChanged when a change takes place
 d3.selectAll("#selDataset").on("change", optionChanged);
 
 
